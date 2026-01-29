de128ad16fa16432a3f98dac00a9e33f39bcab3d
mantrapatel05 <patelmantra551@gmail.com>
2026-01-29 22:00:03 +0530

    Add community creation feature with join/leave functionality, forms, views, and UI

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import page
+from .views import page, communities_view, create_community, join_community, leave_community
 
 urlpatterns = [
     path('', page, {'page_name': 'landing'}, name='landing'),
     path('login/', page, {'page_name': 'login'}, name='login'),
     path('signup/', page, {'page_name': 'signup'}, name='signup'),
+    
+    # Communities
+    path('communities/', communities_view, name='communities'),
+    path('communities/create/', create_community, name='create_community'),
+    path('communities/join/<int:community_id>/', join_community, name='join_community'),
+    path('communities/leave/<int:community_id>/', leave_community, name='leave_community'),
+    
+    # Generic page routing (MUST BE LAST)
     path('<str:page_name>/', page),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
+from django.contrib.auth.decorators import login_required
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Community
 from .serializers import CommunitySerializer
+from .forms import CreateCommunityForm
 
 ALLOWED_PAGES = [
     'landing',
@@ -22,6 +24,47 @@ def page(request, page_name):
     if page_name not in ALLOWED_PAGES:
         raise Http404()
     return render(request, f'{page_name}.html')
+
+
+def communities_view(request):
+    communities = Community.objects.all()
+    return render(request, "communities.html", {
+        "communities": communities
+    })
+
+
+@login_required
+def create_community(request):
+    if request.method == "POST":
+        form = CreateCommunityForm(request.POST, request.FILES)
+        if form.is_valid():
+            community = form.save(commit=False)
+            community.creator = request.user
+            community.save()
+            community.members.add(request.user)
+            return redirect("communities")
+    else:
+        form = CreateCommunityForm()
+
+    return render(request, "create_community.html", {"form": form})
+
+
+@login_required
+def join_community(request, community_id):
+    community = get_object_or_404(Community, id=community_id)
+    if request.method == "POST":
+        if request.user not in community.members.all():
+            community.members.add(request.user)
+    return redirect("communities")
+
+
+@login_required
+def leave_community(request, community_id):
+    community = get_object_or_404(Community, id=community_id)
+    if request.method == "POST":
+        if request.user in community.members.all():
+            community.members.remove(request.user)
+    return redirect("communities")
 
 
 class CommunityViewSet(viewsets.ModelViewSet):
